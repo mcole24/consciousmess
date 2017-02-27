c427d04fcb8a68d75852a9f095920b5d9ba30f33
Matthew Cole <mattaecole@gmail.com>
2017-02-27 16:58:25 +0000

    added test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,95 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ripple.delete_all
+
+Ripple.create(
+        name: 'Matthew Cole',
+        message: 'This is a test message without a URL'
+    )
+    
+Ripple.create(
+        name: 'Matthew\'s Evil Twin',
+        url: 'mattsevillair.com',
+        message: 'This is a test message with a URL'
+    )
+    
+Ripple.create(
+        name: 'Jon Snow',
+        url: 'iknownothing.net',
+        message: 'I apparently know nothing'
+    )
+    
+Ripple.create(
+    name: 'Abigail',
+    
+    message: 'Mauris non dui nec' 
+    )
+    
+    Ripple.create(
+    name: 'Grady',
+    url: 'google.com',
+    message: 'sem molestie sodales. Mauris blandit enim consequat purus.' 
+    )
+    
+    Ripple.create(
+    name: 'Brenden',
+    
+    message: 'scelerisque, lorem ipsum sodales' 
+    )
+    
+    Ripple.create(
+    name: 'Willa',
+    url: 'google.com',
+    message: 'Suspendisse aliquet molestie tellus. Aenean egestas hendrerit' 
+    )
+    
+    Ripple.create(
+    name: 'Freya',
+    
+    message: 'Morbi metus. Vivamus euismod urna.' 
+    )
+    
+    Ripple.create(
+    name: 'Willow',
+    url: 'google.com',
+    message: 'molestie pharetra nibh.' 
+    )
+    
+    Ripple.create(
+    name: 'Charissa',
+    
+    message: 'ornare, facilisis eget, ipsum.' 
+    )
+    
+    Ripple.create(
+    name: 'Cooper',
+    url: 'google.com',
+    message: 'Nulla facilisi.' 
+    )
+    
+    Ripple.create(
+    name: 'Aladdin',
+    url: 'google.com',
+    message: 'ridiculus mus. Proin vel arcu eu odio tristique pharetra. Quisque' 
+    )
+    
+    Ripple.create(
+    name: 'Colby',
+    
+    message: 'Pellentesque habitant morbi tristique senectus et netus' 
+    )
+    
+    Ripple.create(
+    name: 'Jeanette',
+    url: 'google.com',
+    message: 'auctor non, feugiat nec, diam. Duis mi enim,' 
+    )
+    
+    Ripple.create(
+    name: 'Alexandra',
+    
+    message: 'ac, eleifend vitae, erat. Vivamus nisi. Mauris nulla.' 
+    )
+    
+   
