ee63f643a3db9d49ec4e560c04d4a862264f7feb
Matthew Cole <mattaecole@gmail.com>
2017-03-08 21:29:40 +0000

    finished writing tests

--- test/controllers/ripples_controller_test.rb
+++ test/controllers/ripples_controller_test.rb
@@ -21,6 +21,20 @@ class RipplesControllerTest < ActionController::TestCase
     get :previous10
     assert_response(302)
   end
-
   
+  test "should get newest" do 
+    session[:page] = 0
+    get :next10
+    get :newest
+    assert_redirected_to ripples_url
+    assert_equal session[:page], 0
+  end
+  
+  test "should get oldest" do 
+    session[:page] = 1
+    get :previous10
+    get :oldest
+    assert_equal session[:page], (Ripple.all.count / 10)
+  end
+
 end
